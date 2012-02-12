63c185170fde381bf8b751a7ea5593951c5c00b0
hareesh-roxtr <hareesh@roxtr.com>
2012-02-12 23:16:47 +0530

    added logging a bit.

--- src/in/yumtum/lauching/LaunchHelper.java
+++ src/in/yumtum/lauching/LaunchHelper.java
@@ -10,33 +10,45 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
+
 public class LaunchHelper {
+
+	static Logger log = Logger.getLogger(LaunchHelper.class);
 	
     public String insertData(String email){
     	
+    	log.info("In LaunchHelper.insertData");
+    	
     	String result = null;
     	try{		
-    	
+    		log.debug("In try block before getting connection");
     		Connection conn = getDBConnection();
     		PreparedStatement pst = null;
     		String sql = null;
     		ResultSet rs = null;
     		String count = null;
     		conn.setAutoCommit(true);
-    		 
+    		
+    		//log.debug("after getting connection and initializing variables"+conn);
+    		
     		sql = "select count(*) from betausers where email in(?)";
+    		  
+    		  log.debug("before setting prepared statement");
     		  pst = conn.prepareStatement(sql);
     		  pst.setString(1, email);
+    		  log.debug("before execute query");
     		  rs = pst.executeQuery();
-    		  
+    		  log.debug("after execute query");
     		  while (rs.next()) {
     		        count = rs.getString(1);
     		       }
     		  
        		  pst.close();
        		  rs.close();
-    		  
+       		  log.debug("Count should be 1 if email exists: count:"+count);
        		if(count.equals("0")){
+       			log.debug("in If count is 0");
         		java.util.Date dt = new java.util.Date();
         		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
            		String currentTime = sdf.format(dt);
@@ -45,15 +57,19 @@ public class LaunchHelper {
 	    		pst = conn.prepareStatement(sql);
 	    		pst.setString(1, email);
 	    		pst.setString(2, currentTime);
+	    		log.debug("before insert statement execute query");
 	    		pst.executeUpdate();
+	    		log.debug("after insert statement execute query");
 	    		  pst.close();
 	    		  conn.close();
 	    		  result = "success";
        		}else {
+       			log.debug("in else. We will be here if email exists in db already");
        			result = "exists";
        		}
     	}
     	catch (Exception e) {
+    		log.error("exception occoured during sql execution. error is:"+e.getMessage());
     		result = "failure";
     	  }
     	
@@ -68,18 +84,36 @@ public class LaunchHelper {
 		
 		    String DATASOURCE_CONTEXT = "java:comp/env/jdbc/YUMTUM_DS";
 		    
+		    log.info("In LaunchHelper.getDBConnection");
+		    
 		    Connection result = null;
 		    try {
-		    	System.out.println("before getting intial context");
-		      Context initialContext = new InitialContext();
-		      DataSource datasource = (DataSource)initialContext.lookup(DATASOURCE_CONTEXT);
-		      if (datasource != null) {
-		        result = datasource.getConnection();
-		      }
+		    	  log.debug("before creating initial Context");
+			      
+		    	  Context initialContext = new InitialContext();
+			      
+			      log.debug("before creating datasource object");
+			      
+			      DataSource datasource = (DataSource)initialContext.lookup(DATASOURCE_CONTEXT);
+			      
+			      log.debug("after creating datasource object");
+			      
+			      if (datasource != null) {
+			    	  log.debug("before getting connection from DS");
+			        
+			    	  result = datasource.getConnection();
+			          
+			    	  log.debug("after getting connection from DS");
+			    }
 		    }
 		    catch ( NamingException e ) {
+		    	log.error("In NamingException. Message is:"+e.getMessage());
 		    }
 		    catch(SQLException e){
+		    	log.error("In SQLException. Message is:"+e.getMessage());
+		    }
+		    catch(Exception e){
+		    	log.error("In Generic Exception. Message is:"+e.getMessage());
 		    }
 		    return result;
 		  	
--- src/in/yumtum/lauching/saveInfo.java
+++ src/in/yumtum/lauching/saveInfo.java
@@ -8,21 +8,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 /**
  * Servlet implementation class saveInfo
  */
 public class saveInfo extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-     
+	static Logger log = Logger.getLogger(saveInfo.class);
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		// TODO Auto-generated method stub
 	
+		log.info("In do post of saveInfo servlet");
+		
 		String email;
 		
 		email = request.getParameter("email");
+		
+		log.debug("email:"+email);
+		
 		LaunchHelper helper = new LaunchHelper();
 		
 		response.setContentType("text/html");
